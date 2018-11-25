f93d0ad5b2fe8c7e8e0200a1d434415a6502dfb2
Joe Quigley <quigley.joseph@outlook.com>
2018-11-24 23:44:58 -0500

    Add: silly widgets to test dock panel

--- src/mainmenu/index.ts
+++ src/mainmenu/index.ts
@@ -1,5 +1,5 @@
 import { IStratoPlugin } from "../interfaces/plugin";
-import { MenuBar, Menu } from "@phosphor/widgets";
+import { MenuBar, Menu, Widget } from "@phosphor/widgets";
 
 export const MainMenuPlugin: IStratoPlugin<void> = {
     id: "@stratocad/mainmenu",
@@ -23,5 +23,14 @@ export const MainMenuPlugin: IStratoPlugin<void> = {
         })
         menubar.addMenu(file);
         shell.setTopWidget(menubar);
+        const foo = new Widget();
+        foo.node.innerText = "Hi";
+        foo.node.style.backgroundColor = "green";
+        foo.node.style.color = "white";
+        foo.title.label = "foo";
+        const bar = new Widget();
+        foo.node.innerText = "test test 1 2 3";
+        shell.addDockWidget(foo);
+        shell.addDockWidget(bar);
     }
 }
